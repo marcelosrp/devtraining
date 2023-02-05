eafee913ef739ff2fa21d0b7300b42bf05ec7b3f
Marcelo Soares <soares.srp@gmail.com>
2023-02-05 13:11:35 -0300

    Módulo 3: 24. Criando o relacionamento entre as entidades Course e Tag

--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  JoinTable,
+  ManyToMany,
+} from 'typeorm';
+import { Tag } from './tag.entity';
 
 @Entity('courses')
 export class Course {
@@ -11,6 +18,7 @@ export class Course {
   @Column()
   description: string;
 
-  @Column('json', { nullable: true })
+  @JoinTable()
+  @ManyToMany(() => Tag, (tag: Tag) => tag.courses)
   tags: string[];
 }
